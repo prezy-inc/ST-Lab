78ed425206df4621bc796ee3fe98143bdb22afa3
Atsushi <p.taki_atsu_0919@i.softbank.jp>
2021-10-08 19:47:48 +0900

    fixed

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -10,7 +10,6 @@ class MatchesController < ApplicationController
   end
 
   def create
-    @ex_match=Match.create(match_params)
     redirect_to action: :new_match
     @years=Match.pluck(:year).uniq.sort.reverse ##サイドバー
   end
